2115af003a155fbfda6416ed89fbd61de9a46a76
ebllg <erenbilaloglu1@gmail.com>
2022-11-07 23:02:29 +0300

    add eval logic

--- parsing.c
+++ parsing.c
@@ -6,6 +6,56 @@
 
 #include "mpc.h"
 
+/* Use operator string to see which operation to perform */
+long eval_op(long x, char *op, long y)
+{
+    if (strcmp(op, "+") == 0)
+    {
+        return x + y;
+    }
+
+    if (strcmp(op, "-") == 0)
+    {
+        return x - y;
+    }
+
+    if (strcmp(op, "*") == 0)
+    {
+        return x * y;
+    }
+
+    if (strcmp(op, "/") == 0)
+    {
+        return x / y;
+    }
+
+    return 0;
+}
+
+long eval(mpc_ast_t *t)
+{
+    /* If tagged as number return it directly */
+    if (strstr(t->tag, "number"))
+    {
+        return atoi(t->contents);
+    }
+
+    /* The operator is always second child */
+    char *op = t->children[1]->contents;
+
+    /* We store the third child in `x` */
+    long x = eval(t->children[2]);
+
+    /* Iterate the remaining children and combining */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr"))
+    {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+}
+
 int main(int argc, char **argv)
 {
     /* Create some parsers */
@@ -41,13 +91,12 @@ int main(int argc, char **argv)
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r))
         {
-            /* On success print the AST */
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         }
         else
         {
-            /* Otherwise print the error */
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
